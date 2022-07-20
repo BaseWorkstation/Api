3561d4011ab432ba17c98047d18fda257a6002e1
nuel <oyeniyiemmanuel@gmail.com>
2022-07-20 08:24:26 +0100

    update(team members): added unregistered members to the returned team member resource collection

--- app/Http/Resources/TeamMember/TeamMemberCollection.php
+++ app/Http/Resources/TeamMember/TeamMemberCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\TeamMember;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\Models\Team;
 
 class TeamMemberCollection extends ResourceCollection
 {
@@ -14,6 +15,9 @@ class TeamMemberCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'unregistered_members' => Team::find($request->team_id)->unregisteredMembers()->all(),
+        ];
     }
 }
